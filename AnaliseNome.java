e881d5dc8925fb0c1cc054ce07da899e4c14f939
claudiolopesne <37425807+claudiolopesne@users.noreply.github.com>
2019-02-19 21:48:43 -0300

    Create AnaliseNome.java

---
+++ AnaliseNome.java
@@ -0,0 +1,20 @@
+package controle;
+
+import javax.swing.JOptionPane;
+
+public class AnaliseNome {
+	public static void main (String[]args ) {
+		String nome = JOptionPane.showInputDialog("Nome");
+		Integer quantidadeCaracteresNome = nome.length();
+		System.out.println(quantidadeCaracteresNome);
+		
+		String[] palavrasNome=nome.split(" ");
+		System.out.println(palavrasNome[0]);
+		for(int x=0;x<palavrasNome.length; x++) {
+			System.out.println(palavrasNome[x]);
+		}
+	
+	}
+	
+
+}
